6de68ea2665b3fa12e3ed9981048985c993e1a30
JoaoVitorPaludo <joaovpaludo@gmail.com>
2024-05-03 08:40:59 -0300

    style: desenvolvendo a interface da tela de perfil do usuário

--- src/pages/profile/index.page.tsx
+++ src/pages/profile/index.page.tsx
@@ -1,3 +1,11 @@
+import { ButtonComponent } from '../../components/Button/button'
+import * as S from './styles'
 export function ProfilePage() {
-  return <p>profile</p>
+  return (
+    <S.ProfilePageComponent>
+      <img src="https://avatars.githubusercontent.com/u/83378081?v=4" alt="" />
+      <p>João Vitor Primieri Paludo</p>
+      <ButtonComponent text="Sair" />
+    </S.ProfilePageComponent>
+  )
 }
---
+++ src/pages/profile/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const ProfilePageComponent = styled.main`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1.5rem;
+  img {
+    width: 8rem;
+    height: 8rem;
+    border-radius: 4rem;
+  }
+
+  p {
+    font-size: 1.5rem;
+    font-weight: 500;
+    line-height: 1.25rem;
+    letter-spacing: 0.01em;
+    text-align: left;
+  }
+`
