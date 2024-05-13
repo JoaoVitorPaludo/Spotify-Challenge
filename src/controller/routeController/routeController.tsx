5d502337525ca20d288b6dc6e548b206da3a9653
JoaoVitorPaludo <joaovpaludo@gmail.com>
2024-05-13 09:11:05 -0300

    feat: Implementando a autenticação completa e redirecionamento de tela da aplicação

---
+++ src/controller/routeController/routeController.tsx
@@ -0,0 +1,21 @@
+import { api } from '../../service/api'
+
+export const getAcessToken = async (code: string) => {
+  const response = await api.post(
+    'https://accounts.spotify.com/api/token',
+    {
+      grant_type: 'authorization_code',
+      code,
+      redirect_uri: 'http://localhost:5174/callback',
+    },
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${btoa(
+          `${import.meta.env.VITE_CLIENT_ID}:${import.meta.env.VITE_CLIENT_SECRET}`,
+        )}`,
+      },
+    },
+  )
+  return response
+}
--- src/routes/mainRoute.routes.tsx
+++ src/routes/mainRoute.routes.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useCookies } from 'react-cookie'
 import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
 import { SidebarComponent } from '../components/Sidebar/sidebar'
+import { getAcessToken } from '../controller/routeController/routeController'
 import { ArtistsPage } from '../pages/artists/index.page'
 import { HomePage } from '../pages/home/index.page'
 import { LoginPage } from '../pages/login/index.page'
@@ -10,6 +11,8 @@ import { ProfilePage } from '../pages/profile/index.page'
 import { PrivateRoutesContainer } from './styles'
 
 function PrivateRoutes() {
+  const [cookies] = useCookies(['token'])
+  if (!cookies.token) return <LoginPage />
   return (
     <PrivateRoutesContainer>
       <SidebarComponent />
@@ -29,12 +32,17 @@ function ValidateRoutes() {
   const urlParams = new URLSearchParams(window.location.search)
   const code = urlParams.get('code')
 
+  async function getAuthToken(code: string) {
+    try {
+      const { data } = await getAcessToken(code)
+      setCookie('token', data.access_token)
+    } catch (err) {}
+  }
+
   useEffect(() => {
     if (code) {
-      setCookie('token', code, {
-        path: '/',
-      })
-      navigate('/home') // Redireciona para a rota /home
+      getAuthToken(code)
+      navigate('/home')
     } else {
       removeCookie('token')
     }
