06a145021d3f355170378355670ff855ab7581f3
JoaoVitorPaludo <joaovpaludo@gmail.com>
2024-07-12 09:22:06 -0300

    feat: Implementando as configurações do PWA

--- src/pages/login/index.page.tsx
+++ src/pages/login/index.page.tsx
@@ -4,7 +4,7 @@ import * as S from './styles'
 export function LoginPage() {
   function handleAuthPage() {
     // window.location.href = `${import.meta.env.VITE_API_URL}/authorize?client_id=${import.meta.env.VITE_CLIENT_ID}&redirect_uri=http://localhost:5174/callback&response_type=code`
-    window.location.href = `${import.meta.env.VITE_API_URL}/authorize?client_id=${import.meta.env.VITE_CLIENT_ID}&redirect_uri=http://localhost:5174/callback&response_type=code&scope=user-top-read playlist-modify-public playlist-modify-private user-read-recently-played`
+    window.location.href = `${import.meta.env.VITE_API_URL}/authorize?client_id=${import.meta.env.VITE_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_REDIRECT_URL}&response_type=code&scope=user-top-read playlist-modify-public playlist-modify-private user-read-recently-played`
   }
   return (
     <S.LoginContainer>
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
@@ -27,7 +27,62 @@ export default defineConfig({
       '**/src/styles/global.ts**',
     ],
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Spotify Integration',
+        short_name: 'Spotify App',
+        description: 'My integration with Spotify',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/api\.spotify\.com\/v1\/.*/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'spotify-api-cache',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 60 * 24, // 1 dia
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+          {
+            urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'image-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 dias
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 5174,
   },
